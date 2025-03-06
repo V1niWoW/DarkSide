8bf6bbb0f61ebfcbf04aa3d07ec2a3c9bc920611
V1niWoW <viniciosbernardi12@gmail.com>
2025-03-05 21:34:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-// Exemplo de animação simples ao passar o mouse sobre o botão
-const ctaButton = document.querySelector('.cta-button');
-
-ctaButton.addEventListener('mouseover', () => {
-    ctaButton.style.transform = 'scale(1.1)';
-});
-
-ctaButton.addEventListener('mouseout', () => {
-    ctaButton.style.transform = 'scale(1)';
-});
+// script.js (opcional, para funcionalidades adicionais)
+console.log("Site carregado com sucesso!");
